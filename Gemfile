a418cfdb63bc2823b2c3f743f4cebbe7001a1024
Jason <jasonkwong11@gmail.com>
2016-10-13 15:16:39 -0400

    more devise config and setting views

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'jquery-turbolinks'
 gem 'devise'
 gem 'bootstrap-sass'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get '/secret', to: 'posts#secret', as: :secret
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   resources :posts
