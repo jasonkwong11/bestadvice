94310d91fc279fe140bc08ac7bec9ee25cd26f0c
Jason <jasonkwong11@gmail.com>
2016-10-13 17:01:33 -0400

    added route to twitter oauth login and view buttons

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['9FJMffHfmzZtKx3Yl6jFp8i7D'], ENV['RR0GHi30rz1yPPpJjsuWliIi6bZ9tIT1ybdv4vgnet5hzFJ1g5']
+  provider :twitter, '9FJMffHfmzZtKx3Yl6jFp8i7D', 'RR0GHi30rz1yPPpJjsuWliIi6bZ9tIT1ybdv4vgnet5hzFJ1g5'
 end
