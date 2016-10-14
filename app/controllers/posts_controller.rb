498ba63512eb983039f063227dad7d7acd441708
Jason <jasonkwong11@gmail.com>
2016-10-14 19:44:02 -0400

    debugged posts#create issue on why it wasn't saving to db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,30 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to :root
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
-  def new
-    @post = Post.new
-  end
 end
+
+
+private
+  
+  def post_params
+    params.require(:post).permit!
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/posts/new', to: 'posts#new', as: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   resources :posts
+
+ # get '/posts/new', to: 'posts#new', as: :new
 end
